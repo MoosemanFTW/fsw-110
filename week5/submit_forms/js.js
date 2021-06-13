a75e47649af347a644905515a673f650f8a15a0b
MoosemanFTW <justin.milligan@bryanuniversity.edu>
2021-06-13 16:27:50 -0700

    fixed 2 issues

--- week5/submit_forms/js.js
+++ week5/submit_forms/js.js
@@ -1,9 +1,9 @@
 var fn = document.getElementById('fname')
 var ln = document.getElementById('lname')
 var age = document.getElementById('age')
-var q1 = document.getElementsByName('Q1')
-var q2 = document.getElementsByName('Q2')
-var q3 = document.getElementsByName('Q3')
+var q1 = document.Q1
+var q2 = document.Q2
+var q3 = document.Q3.checked
 var submit = document.getElementById('button')
 
 
@@ -13,7 +13,7 @@ submit.addEventListener('click', function(event){
     alert('First Name: ' + fn.value + '\n' + 
     'Last Name: ' + ln.value + '\n' +
     'Age: ' + age.value + '\n' +
-    'Shopped before: ' + q1.value + '\n' +
-    'Recommend us: ' + q2.value + '\n' +
-    'Favorite product: ' + q3.value + '\n')
+    'Shopped before: ' + q1.Q1.value + '\n' +
+    'Recommend us: ' + q2.Q2.value + '\n' +
+    'Favorite product: ' + q3.Q3.value + '\n')
 })
